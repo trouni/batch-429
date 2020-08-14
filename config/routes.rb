f0af4f1f3076e783a44647778b6b9966e8d890cf
Trouni Tiet <trouni@gmail.com>
2020-08-14 09:26:39 +0900

    Add review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :restaurants, only: [ :index, :show ]
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
 end
